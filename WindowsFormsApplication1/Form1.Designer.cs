1c9cea68ee45d103f41b340bab54a5091abecc85
Paakallo <paviveerar@gmail.com>
2025-06-29 12:00:25 +0200

    final changes from lab

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,31 +29,36 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Start = new System.Windows.Forms.Button();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.COM_port = new System.Windows.Forms.Label();
-            this.Read_value = new System.Windows.Forms.Label();
             this.Confirm = new System.Windows.Forms.Button();
             this.Stop = new System.Windows.Forms.Button();
             this.Data = new System.Windows.Forms.TextBox();
             this.Close = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Data1 = new System.Windows.Forms.TextBox();
+            this.Unit = new System.Windows.Forms.TextBox();
+            this.refresh = new System.Windows.Forms.Timer(this.components);
+            this.measurementTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Start
             // 
-            this.Start.Location = new System.Drawing.Point(469, 128);
+            this.Start.BackColor = System.Drawing.Color.Lime;
+            this.Start.Location = new System.Drawing.Point(438, 128);
             this.Start.Name = "Start";
             this.Start.Size = new System.Drawing.Size(75, 23);
             this.Start.TabIndex = 0;
             this.Start.Text = "Start";
-            this.Start.UseVisualStyleBackColor = true;
+            this.Start.UseVisualStyleBackColor = false;
             this.Start.Click += new System.EventHandler(this.komunizm);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(165, 128);
+            this.textBox1.Location = new System.Drawing.Point(333, 130);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(77, 20);
             this.textBox1.TabIndex = 2;
@@ -63,25 +68,16 @@
             // COM_port
             // 
             this.COM_port.AutoSize = true;
-            this.COM_port.Location = new System.Drawing.Point(179, 99);
+            this.COM_port.Location = new System.Drawing.Point(343, 105);
             this.COM_port.Name = "COM_port";
             this.COM_port.Size = new System.Drawing.Size(63, 13);
             this.COM_port.TabIndex = 3;
             this.COM_port.Text = "Current Port";
             this.COM_port.Click += new System.EventHandler(this.COM_port_Click);
             // 
-            // Read_value
-            // 
-            this.Read_value.AutoSize = true;
-            this.Read_value.Location = new System.Drawing.Point(492, 98);
-            this.Read_value.Name = "Read_value";
-            this.Read_value.Size = new System.Drawing.Size(33, 13);
-            this.Read_value.TabIndex = 4;
-            this.Read_value.Text = "None";
-            // 
             // Confirm
             // 
-            this.Confirm.Location = new System.Drawing.Point(165, 170);
+            this.Confirm.Location = new System.Drawing.Point(331, 173);
             this.Confirm.Name = "Confirm";
             this.Confirm.Size = new System.Drawing.Size(75, 23);
             this.Confirm.TabIndex = 5;
@@ -91,26 +87,27 @@
             // 
             // Stop
             // 
-            this.Stop.Location = new System.Drawing.Point(613, 128);
+            this.Stop.BackColor = System.Drawing.Color.Red;
+            this.Stop.Location = new System.Drawing.Point(533, 128);
             this.Stop.Name = "Stop";
             this.Stop.Size = new System.Drawing.Size(75, 23);
             this.Stop.TabIndex = 6;
             this.Stop.Text = "Stop";
-            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.UseVisualStyleBackColor = false;
             this.Stop.Click += new System.EventHandler(this.Stop_Click);
             // 
             // Data
             // 
-            this.Data.Location = new System.Drawing.Point(593, 83);
+            this.Data.Location = new System.Drawing.Point(333, 68);
             this.Data.Name = "Data";
             this.Data.ReadOnly = true;
-            this.Data.Size = new System.Drawing.Size(100, 20);
+            this.Data.Size = new System.Drawing.Size(218, 20);
             this.Data.TabIndex = 7;
             this.Data.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Close
             // 
-            this.Close.Location = new System.Drawing.Point(620, 215);
+            this.Close.Location = new System.Drawing.Point(533, 173);
             this.Close.Name = "Close";
             this.Close.Size = new System.Drawing.Size(75, 23);
             this.Close.TabIndex = 8;
@@ -122,16 +119,32 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // Data1
+            // 
+            this.Data1.Location = new System.Drawing.Point(333, 23);
+            this.Data1.Name = "Data1";
+            this.Data1.Size = new System.Drawing.Size(218, 20);
+            this.Data1.TabIndex = 9;
+            // 
+            // Unit
+            // 
+            this.Unit.Location = new System.Drawing.Point(569, 23);
+            this.Unit.Name = "Unit";
+            this.Unit.Size = new System.Drawing.Size(39, 20);
+            this.Unit.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(718, 392);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(888, 371);
+            this.Controls.Add(this.Unit);
+            this.Controls.Add(this.Data1);
             this.Controls.Add(this.Close);
             this.Controls.Add(this.Data);
             this.Controls.Add(this.Stop);
             this.Controls.Add(this.Confirm);
-            this.Controls.Add(this.Read_value);
             this.Controls.Add(this.COM_port);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Start);
@@ -149,12 +162,15 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label COM_port;
-        private System.Windows.Forms.Label Read_value;
         private System.Windows.Forms.Button Confirm;
         private System.Windows.Forms.Button Stop;
         private System.Windows.Forms.TextBox Data;
         private System.Windows.Forms.Button Close;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox Data1;
+        private System.Windows.Forms.TextBox Unit;
+        private System.Windows.Forms.Timer refresh;
+        private System.Windows.Forms.Timer measurementTimer;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -13,21 +14,32 @@ namespace WindowsFormsApplication1
     {
         private string COM_p;
         bool rtsState;
+        byte[] buffer1;
 
         public Form1()
         {
             InitializeComponent();
             serialPort1 = new System.IO.Ports.SerialPort();
-            // serialPort1.PortName = "COM5";
             serialPort1.BaudRate = 9600;
             serialPort1.StopBits = System.IO.Ports.StopBits.One;
             serialPort1.DataBits = 8;
             serialPort1.Parity = System.IO.Ports.Parity.None;
 
-            // receive data in a different thread
-            // serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceivedHandler);
+            measurementTimer = new System.Windows.Forms.Timer();
+            measurementTimer.Interval = 1000; // co 1 sekunda
+            measurementTimer.Tick += MeasurementTimer_Tick;
+
 
         }
+
+        private void MeasurementTimer_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                read_data();
+            }
+        }
+
 
         private void DataReceivedHandler(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
@@ -45,8 +57,6 @@ namespace WindowsFormsApplication1
 
         }
 
-     
-        
         // assign COM port and change label
         private void Confirm_Click(object sender, EventArgs e)
         {
@@ -59,26 +69,20 @@ namespace WindowsFormsApplication1
         // stop recording
         private void Stop_Click(object sender, EventArgs e)
         {
+            serialPort1.Close();
+            Data1.Text = "";
+            Data.Text = "";
+            Unit.Text = "";
 
-        }
-
-        // start recording
-        private void Start_Click_1(object sender, EventArgs e)
-        {
-            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1000;
-            Data.Text = "Otwieram";
-            
-            //timer.Start();
-            //System.Threading.Thread.Sleep(1000);
-            //serialPort1.Open();
-            //System.Threading.Thread.Sleep(1000);
-            Data.Text = "Czytam";
-            //serialPort1.Close();
+            measurementTimer.Stop();
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
          }
 
 
-        public void komunizm(object sender, EventArgs e)
+        public void init_rts()
         {
             serialPort1.ReadTimeout = 5000; //read timeout
             serialPort1.WriteTimeout = 200; //write timeout
@@ -87,9 +91,6 @@ namespace WindowsFormsApplication1
 
             serialPort1.Open();
             System.Threading.Thread.Sleep(100);
-            //timer1.Interval = 1000;
-            //timer1.Tick += timer1_Tick;
-            //timer1.Start();
 
             serialPort1.RtsEnable = true;
             System.Threading.Thread.Sleep(100);
@@ -97,56 +98,97 @@ namespace WindowsFormsApplication1
             System.Threading.Thread.Sleep(100);
             serialPort1.RtsEnable = true;
             System.Threading.Thread.Sleep(100);
-
-
-            bool close_con = true;
-
-            string exanple = "$10$02$00$00$00$00$10$03";
-
-            int data;
-            int[] read_data;
-            byte[] bytes = new byte[] {0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x10, 0x03};
-          
-            //{ 00010000, 00000010, 00000000, 00000000, 00000000, 00000000, 00010000, 00000011 };
-            serialPort1.Write(bytes, 0, bytes.Length);
-
-            
-
-            for (int i =0; i<22; i++){
-                data = serialPort1.ReadByte();
-                read_data[i] = data; //pozniej dokonczymy!!!
-                Data.AppendText(data.ToString());
-            }
-            //22
-
-            //ToString(serialPort1.ReadExisting())
-            //response = bytes.ToString() ;
-            //Data.AppendText(response);
-            //Data.Text = response;
-
-            int len = bytes.Length;
-            //Data.Text = len.ToString();
-            // Data.Text = BitConverter.ToString(bytes2);
-            if (close_con)
-            {
-                serialPort1.Close();
-            }
         }
+
+
+        public void read_data()
+        {
+            Data1.Text = "";
+            Data.Text = "";
+            Unit.Text = "";
+            int[] read_data = new int[] { };
+            byte[] bytes = new byte[] { 0x10, 0x02, 0x00, 0x00, 0x00, 0x00, 0x10, 0x03 };
+
+            //{ 00010000, 00000010, 00000000, 00000000, 00000000, 00000000, 00010000, 00000011 };
+
+            // Send command to Sanwa
+            serialPort1.Write(bytes, 0, bytes.Length);
+                        
+            byte[] buffer = new byte[22];
+            buffer1 = buffer;
+
+
+            int bytesRead = 0;
+
+            while (bytesRead < 22)
+            {
+                bytesRead += serialPort1.Read(buffer, bytesRead, 22 - bytesRead);
+            }
+
+            string hex = BitConverter.ToString(buffer);
+            Data.AppendText(hex + Environment.NewLine);
+
+            string asciiPart = "";
+            for (int i = 8; i <= 18; i++)
+            {
+                asciiPart += (char)buffer[i];
+            }
+
+            Data1.AppendText(asciiPart);
+            assignUnit();
+            Save_Click(null, null);
+        }
+
+        // start recording
+        public void komunizm(object sender, EventArgs e)
+        {
+            init_rts();
+            measurementTimer.Start();
+        }
+
+
+        // assign unit according to multimeter option
+        private void assignUnit()
+        {
+            int byteUnit = buffer1[4];
+
+            switch (byteUnit)
+            {
+                case 128:
+                    Unit.Text = "Ohm";
+                    break;
+                case 05:
+                    Unit.Text = "AcV";
+                    break;
+                case 06:
+                    Unit.Text = "DcV";
+                    break;
+                case 1:
+                    Unit.Text = "AcA";
+                    break;
+                case 2:
+                    Unit.Text = "DcA";
+                    break;
+                case 0:
+                    Unit.Text = "Hz";
+                    break;
+
+            }
+
+        }
+
 
         private void COM_port_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Data_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void Close_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
@@ -154,6 +196,15 @@ namespace WindowsFormsApplication1
             serialPort1.RtsEnable = rtsState;
         }
 
-
+        // add current value to the file
+        private void Save_Click(object sender, EventArgs e)
+        {
+            using (FileStream fs = new FileStream("dane.txt", FileMode.Append, FileAccess.Write))
+            {
+                string linia = "Wartosc: " + Data1.Text + Unit.Text + Environment.NewLine;
+                byte[] dane = System.Text.Encoding.ASCII.GetBytes(linia);
+                fs.Write(dane, 0, dane.Length);
+            }
+        }
     }
 }
